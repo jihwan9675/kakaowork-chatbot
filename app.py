6ffecede4743af3b57c8c3d29deb34e92715065c
Shinjihwan <jihwan9675@gmail.com>
2021-05-03 13:53:21 +0900

    send_result_message 함수 선언 make_users_array -> get_userNames [함수명 변경] get_uersIds 함수 선언

--- app.py
+++ app.py
@@ -9,14 +9,15 @@ from models import db, Complement
 
 app = Flask(__name__)
 
+# Modal Json 파일 읽어오기
 with open('modal.json', 'r') as f:
     json_data = json.load(f)
 
 
+# 칭찬 내역 DB에 Insert
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        #print(request.data.decode('utf-8'))
         res = json.loads(request.data.decode('utf-8'))
         print(res['actions'])
 
@@ -27,8 +28,8 @@ def index():
         complement.complement_value = '칭찬'
         complement.complement_content = '얘는 착하다.'
         #complement.id = res['actions']['userName']
-        #complement.senderid = res['message']['userid']
-        #complement.complement_value = res['message']['complement_value']
+        #complement.senderid = res['message']['user']['user_id']
+        ##complement.complement_value = res['message']['complement_value']
         ##complement.complement_content = res['message']['complement_content']
         db.session.add(complement)
         db.session.commit()
@@ -36,17 +37,20 @@ def index():
     return "s"
 
 
+# modal 요청이 오면 json 파일 리턴
 @app.route('/modal', methods=['GET', 'POST'])
 def modal():
     if request.method == 'POST':
         print(request.data['type'])
-        json_data['blocks'][1]['options'] = make_users_array()
+        json_data['blocks'][1]['options'] = get_userNames()
     return json_data
 
 
+# 칭찬 횟수 조회
 @app.route('/search', methods=['GET', 'POST'])
 def search():
-    if request.method == 'GET':
+    if request.method == 'POST':
+        
         my_complement_count = Complement.query.filter_by(id=1).count() # 칭찬받은 횟수
         complemented = Complement.query.filter_by(senderid=1) # 칭찬한 횟수
         complemented_array = []
@@ -56,8 +60,18 @@ def search():
         
     return json_data
 
+# 유저 중 한명이 칭찬을 했을 경우 단톡에 결과 메시지를 보냄
+def send_result_message():
+    URL = 'https://api.kakaowork.com/v1/conversations.open'
+    headers = {'Content-Type': 'application/json;',
+               'Authorization': 'Bearer c856d570.9901b06a691f4c3da28d3ad830a4e658'}
+    data = {'user_ids':get_userIds()}
+    res = requests.post(URL, headers=headers,data=data)
+    print(res)
 
-def make_users_array():
+
+# 유저 이름과 인덱스를 매칭한 딕셔너리를 담는 배열 생성
+def get_userNames():
     URL = 'https://api.kakaowork.com/v1/users.list'
     headers = {'Content-Type': 'application/json; charset=utf-8',
                'Authorization': 'Bearer c856d570.9901b06a691f4c3da28d3ad830a4e658'}
@@ -68,6 +82,20 @@ def make_users_array():
         users.append({"text": user['name'], "value": i})
 
     return users
+
+# 유저 id 배열 생성
+def get_userIds():
+    URL = 'https://api.kakaowork.com/v1/users.list'
+    headers = {'Content-Type': 'application/json; charset=utf-8',
+               'Authorization': 'Bearer c856d570.9901b06a691f4c3da28d3ad830a4e658'}
+    res = requests.get(URL, headers=headers)
+
+    userids = []
+    for user in res.json()['users']:
+        userids.append(user['id'])
+
+    return userids
+
 
 
 # DB Setting
